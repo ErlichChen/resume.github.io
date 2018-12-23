9a646fee3a6db5d3fd4add39d7bb19cdad696869
erlichchen <chenxuewei.dev@gmail.com>
2018-12-23 18:01:11 +0800

    add button

---
+++ js/index.js
@@ -0,0 +1,10 @@
+(function(global){
+    'use strict';
+
+    function showDialog() {
+        global.alert("Hello ")
+    }
+
+    document.querySelector('.firstbutton').addEventListener('click', showDialog, false);
+
+})(window)
