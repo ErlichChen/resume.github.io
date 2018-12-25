be414db5137da65abd5b114570d42be9cf7e8d28
erlichchen <chenxuewei.dev@gmail.com>
2018-12-25 09:53:01 +0800

    add click listener for arrow

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,34 @@
 (function(global){
     'use strict';
 
-    function showDialog() {
-        global.alert("Hello")
-    }
+		function eventHandler(event) {
+			switch(event.type) {
+				case 'click':
+					handleClick(event.target)
+				break
+				default:
+				break
+			}
+		}
 
-    document.querySelector('.firstbutton').addEventListener('click', showDialog, false);
+		function handleClick(target) {
+			switch(target.className) {
+				case 'photo':
+				console.log(target.className);
+				break;
+				case 'arrow-right':
+				console.log(target.className);
+				break;
+				case 'arrow-down':
+				console.log(target.className);
+				break;
+			}
+
+		}
+
+
+		document.querySelector('.photo-btn').addEventListener('click', eventHandler, false);
+		document.querySelector('.arrow-right-btn').addEventListener('click', eventHandler, false);
+		document.querySelector('.arrow-down-btn').addEventListener('click', eventHandler, false);
 
 })(window)
